18d3db5d906c43a17a4190481ca35deac2b9cc98
Lineaje DepFixer <221268890+Lineaje-DepFixer@users.noreply.github.com>
2026-04-22 08:08:30 +0000

    Update manifest
    Update manifest to address vulnerabilities

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     packages=find_packages(exclude=["tests", "tests.*"]),
     python_requires=">=3.7, <4",
     install_requires=[
-        "urllib3==1.26.18",
+        "urllib3==1.26.20+lineaje-01",
         "python-dateutil==2.8.2",
     ],
 )
